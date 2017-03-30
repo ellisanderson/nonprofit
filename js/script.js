01c251357f9ee005372d681820532a13b1978b02
ellisanderson <ellishenryanderson@gmail.com>
2017-03-30 16:27:56 -0400

    added topbox/button styles and jQuery functionality

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@ $(document).ready(function(){
     $(this).next('ul').slideToggle();
   });
 });
+
+$(document).ready(function(){
+  $('.top-button').click(function(){
+    $('html, body').animate({scrollTop : 0},800);
+		return false;
+  });
+});
