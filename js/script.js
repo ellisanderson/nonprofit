5f15f8f558c07017b18efb4755d6456fcaf38904
ellisanderson <ellishenryanderson@gmail.com>
2017-04-03 16:40:40 -0400

    merged html, began global styling

--- js/script.js
+++ js/script.js
@@ -10,3 +10,34 @@ $(document).ready(function(){
 		return false;
   });
 });
+
+$(document).ready(function(){
+  $('.exhibits-carousel').slick({
+    // centerMode: true,
+    dots: true,
+    arrows: false,
+    appendArrows: $('.left'),
+  });
+});
+$('.left').on('click', function() {
+  $('.exhibits-carousel').slick('slickPrev');
+});
+$('.right').on('click', function() {
+  $('.exhibits-carousel').slick('slickNext');
+});
+
+window.addEventListener('scroll', function(e){
+    var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+        shrinkOn = 300,
+        header = document.querySelector("header"),
+        nav = document.querySelector(".nav");
+    if (distanceY > shrinkOn) {
+        classie.add(header,"smaller");
+        classie.add(nav,"smaller-nav");
+    } else {
+        if (classie.has(header,"smaller")) {
+            classie.remove(header,"smaller");
+            classie.remove(nav,"smaller-nav");
+        }
+    }
+});
